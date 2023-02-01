2df05de4735043d5ced807f35df3ba510f2d0fe4
Robert Beck <hbeck35@googlemail.com>
2023-02-01 10:36:38 +0100

    added marker functionality

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "../css/map.css";
 
-const center = [52.51629872917535, 13.37805398629472];
-
 const Map = () => {
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      position => setUserLocation([position.coords.latitude, position.coords.longitude])
+    );
+  }, []);
+
+  let center = userLocation || [52.51629872917535, 13.37805398629472];
+
   return (
     <MapContainer
       center={center}
@@ -16,11 +28,13 @@ const Map = () => {
         url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=5sU25nOT7O8fAUXuiaYf"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-      <Marker position={center}>
-        <Popup>
-          Brandenburger Tor <br /> Klicke für mehr Informationen.
-        </Popup>
-      </Marker>
+      {userLocation && (
+        <Marker position={userLocation}>
+          <Popup>
+            Deine aktuelle Position
+          </Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 };
