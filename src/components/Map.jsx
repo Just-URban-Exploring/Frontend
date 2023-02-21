c9a5e045cde28be418fc212097b29bb232701a6c
Robert Beck <hbeck35@googlemail.com>
2023-02-21 13:52:02 +0100

    added jump-to location animation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import * as geolib from "geolib";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import Markers from "./Markers.jsx";
+import L from "leaflet";
 import { markers } from "./Markers";
 import "leaflet/dist/leaflet.css";
 import "../css/Map.css";
@@ -18,17 +19,21 @@ const Map = () => {
   const [markerIcons, setMarkerIcons] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
   const prevNearestMarker = useRef(null);
+  const mapRef = useRef(null);
+  const map = mapRef.current;
 
-  const handleGetLocation = () => { // get location
+  const handleGetLocation = () => {
     if (!navigator.geolocation) {
       alert("Geolocation is not supported by your browser");
     } else {
       navigator.geolocation.getCurrentPosition(
-        (position) =>
-          setUserLocation({ //stellt user position
+        (position) => {
+          setUserLocation({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
-          }),
+          });
+          map.flyTo([position.coords.latitude, position.coords.longitude], 17);
+        },
         (error) => console.error(error),
         {
           enableHighAccuracy: true,
@@ -39,35 +44,33 @@ const Map = () => {
     }
   };
 
-  const handleGoToBerlin = () => { //custom marker für de-bugging
-    setUserLocation({
-      latitude: 52.516275,
-      longitude: 13.377704
-    });
+  const handleGoToBerlin = () => {
+    setCurrentLatLng([52.516275, 13.377704]);
     setNearestMarkers([]);
+    map.flyTo([52.516275, 13.377704], 18);
   };
 
   const handleMarkerClick = (marker) => {
     setSelectedMarker(marker);
   };
 
-  useEffect(() => { 
+  useEffect(() => {
     if (userLocation) {
       const nearestMarkers = markers
         .map((marker) => ({
           ...marker,
-          distance: Math.min( // ab hier rechnung für distanz
+          distance: Math.min(
             geolib.getDistance(userLocation, marker.location)
           ),
         }))
         .sort((a, b) => a.distance - b.distance)
         .slice(0, 5);
       setNearestMarkers(nearestMarkers);
-
-      const icons = nearestMarkers.map((marker) => marker.icon); //setzt icons
+  
+      const icons = nearestMarkers.map((marker) => marker.icon);
       setMarkerIcons(icons);
-
-      const nearestMarker = nearestMarkers[0]; //funktion für Vibration
+  
+      const nearestMarker = nearestMarkers[0];
       if (
         nearestMarker.distance <= 10 &&
         nearestMarker !== prevNearestMarker.current
@@ -75,21 +78,36 @@ const Map = () => {
         if (navigator.vibrate) {
           navigator.vibrate(VIBRATION_DURATION);
   
-          
-          setTimeout(() => { // Code to test the vibration
-            navigator.vibrate(0); // stop the vibration
-            alert("DEIN GERÄT VIBRIERT JUNGE!!!!"); // show an alert (sollte akkurat sein für die funktion)
+          setTimeout(() => {
+            navigator.vibrate(0);
+            alert("DEIN GERÄT VIBRIERT JUNGE!!!!");
           }, VIBRATION_DURATION);
         }
         prevNearestMarker.current = nearestMarker;
       }
+    } else if (nearestMarkers.length === 0) { // recalculate nearest markers for new location
+      const nearestMarkers = markers
+        .map((marker) => ({
+          ...marker,
+          distance: Math.min(
+            geolib.getDistance(currentLatLng, marker.location)
+          ),
+        }))
+        .sort((a, b) => a.distance - b.distance)
+        .slice(0, 5);
+      setNearestMarkers(nearestMarkers);
+  
+      const icons = nearestMarkers.map((marker) => marker.icon);
+      setMarkerIcons(icons);
     }
-  }, [markers, userLocation]);
+  }, [markers, userLocation, currentLatLng, nearestMarkers]);
+
   return (
     <div>
       <button onClick={handleGetLocation}>Zu deiner Position</button>
       <button onClick={handleGoToBerlin}>Besuche Berlin!</button>
       <MapContainer
+        ref={mapRef}
         center={currentLatLng}
         zoom={19}
         style={{ width: "100vw", height: "80vh" }}
