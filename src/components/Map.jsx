3002c0d8887e1a325431d2d5bdbec6d831bcca35
Robert Beck <hbeck35@googlemail.com>
2023-02-21 14:36:40 +0100

    added marker debug

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -32,7 +32,7 @@ const Map = () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
           });
-          map.flyTo([position.coords.latitude, position.coords.longitude], 17);
+          map.flyTo([position.coords.latitude, position.coords.longitude], 18);
         },
         (error) => console.error(error),
         {
@@ -45,9 +45,10 @@ const Map = () => {
   };
 
   const handleGoToBerlin = () => {
-    setCurrentLatLng([52.516275, 13.377704]);
+    const berlinLocation = [52.516275, 13.377704];
+    setCurrentLatLng(berlinLocation);
     setNearestMarkers([]);
-    map.flyTo([52.516275, 13.377704], 18);
+    map.flyTo(berlinLocation, 19);
   };
 
   const handleMarkerClick = (marker) => {
@@ -96,7 +97,6 @@ const Map = () => {
         .sort((a, b) => a.distance - b.distance)
         .slice(0, 5);
       setNearestMarkers(nearestMarkers);
-  
       const icons = nearestMarkers.map((marker) => marker.icon);
       setMarkerIcons(icons);
     }
@@ -107,25 +107,30 @@ const Map = () => {
       <button onClick={handleGetLocation}>Zu deiner Position</button>
       <button onClick={handleGoToBerlin}>Besuche Berlin!</button>
       <MapContainer
-        ref={mapRef}
-        center={currentLatLng}
-        zoom={19}
-        style={{ width: "100vw", height: "80vh" }}
-      >
-        <TileLayer
-          url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=5sU25nOT7O8fAUXuiaYf"
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-        />
-        <Markers markers={markers} onMarkerClick={handleMarkerClick} />
-        {userLocation ? (
-          <Marker
-            position={[userLocation.latitude, userLocation.longitude]}
-            key="user"
-          >
-            <Popup>Deine aktuelle Position</Popup>
-          </Marker>
-        ) : null}
-      </MapContainer>
+  ref={mapRef}
+  center={currentLatLng}
+  zoom={19}
+  style={{ width: "100vw", height: "80vh" }}
+>
+  <TileLayer
+    url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=5sU25nOT7O8fAUXuiaYf"
+    attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+  />
+  <Markers markers={markers} onMarkerClick={handleMarkerClick} />
+  {userLocation ? (
+    <Marker
+      position={[userLocation.latitude, userLocation.longitude]}
+      key="user"
+    >
+      <Popup>Deine aktuelle Position</Popup>
+    </Marker>
+  ) : null}
+  {currentLatLng && (
+    <Marker position={currentLatLng} key="current">
+      <Popup>Besuche Berlin!</Popup>
+    </Marker>
+  )}
+</MapContainer>
       {nearestMarkers.length > 0 ? (
         <div className="marker-popup">
           <ul>
