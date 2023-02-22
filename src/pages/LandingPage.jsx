811f9bf6fd2a7138d35113d97cf578a1056c37ca
Robert Beck <hbeck35@googlemail.com>
2023-02-22 15:26:53 +0100

    added styling

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,18 +5,23 @@ import Icons from "../assets/Icons.svg";
 import Map from "../assets/Map.svg";
 import SecretIcon from "../assets/SecretIcon.svg";
 import AudioGuide from "../assets/AudioGuide.svg";
+
 export function LandingPage() {
   return (
     <div className={styles.container}>
+      <div className={styles.topBar}>
+        {/* <button className={styles.topBarButton}>Jetzt registrieren</button> */}
+      </div>
+
       <div className={styles.containerWillkommen}>
-        <h2>Willkommen</h2>
+        <h1>Willkommen bei JUst Urban Exploring</h1>
         <img src={Jure} alt="Logo" />
         <div className={styles.jure}>
-          <p className={styles.jureParagraf}>
+          <h1 className={styles.jureParagraf}>
             <b>JU</b>st <b>UR</b>ban <b>E</b>xploring
-          </p>
+          </h1>
           <div>
-            <p>Alles, aber simpel</p>
+            <h2 className={styles.jureParagraf}>Alles, aber simpel</h2>
           </div>
         </div>
       </div>
@@ -24,7 +29,7 @@ export function LandingPage() {
       <div className={styles.containerTakeATour}>
         {<img src={Berlin} alt="Berlin" />}
 
-        <h2 className={styles.containerTakeATourH2}>Take a Tour</h2>
+        <h2 className={styles.containerTakeATourH2}>Erkunde die Welt auf deine eigene Art und Weise</h2>
         <div className={styles.containerTakeATourParagraf}>
           <p>
             Sei dein eigener Tour Guide. Besuche die interessantesten Orte der
@@ -42,7 +47,7 @@ export function LandingPage() {
             <img src={SecretIcon} alt="Secret Icon" />
           </div>
           <p>
-            Dank der Icons kannst du auf einen Blick sehen was sehenswert ist.
+            Dank der Icons kannst du auf einen Blick sehen, was sehenswert ist.
             Klicke auf ein Icon und lasse dir Informationen zu dem ausgewählten
             Ort anzeigen. Jeder Ort hat seine eigene Geschichte.
           </p>
@@ -50,38 +55,38 @@ export function LandingPage() {
       </div>
 
       <div className={styles.containerMap}>
-        {<img src={Map} alt="Berlin" />}
-        <h2 className={styles.containerMapH2}>Map</h2>
+        {<img src={Map} alt="Map" />}
+        <h2 className={styles.containerMapH2}>Karte</h2>
         <div className={styles.containerMapParagraf}>
           <p>
             Museen, schöne Aussichten, Parks, Cafés und sogar das nächstgelegene
             WC werden dir auf der Karte angezeigt. Auf deinem Weg durch die Orte
-            der Welt entscheidest du wo es lang geht.
+            der Welt entscheidest du, wo es lang geht.
           </p>
         </div>
       </div>
+
       <div className={styles.containerAudioGuide}>
-        <img src={AudioGuide} alt="Logo" />
+        <img src={AudioGuide} alt="AudioGuide" />
         <div>
-          <p>
+          <p className={styles.audioGuideText}>
             Bist du eher der auditive Entdecker? Lass dir die Tour vorlesen mit
             einem Klick kannst du in eine andere Zeit oder neue Welt eintauchen.
           </p>
         </div>
       </div>
 
-      <div className={styles.containercallBackAction}>
-        <h2>Just do It</h2>
-        <img src={Jure} alt="Logo" />
-        <div className={styles.callBackActionParagraf}>
-          <p className={styles.callBackActionJure}>
-            <b>JU</b>st <b>UR</b>ban <b>E</b>xploring
-          </p>
-          <div>
-            <p>Alles, aber simpel</p>
-          </div>
-        </div>
-        <button className={styles.callbackActionButton}>Anmelden</button>
+      <div className={styles.containerCallBackAction}>
+        <h2>Jetzt loslegen</h2>
+        <p>
+          Registriere dich jetzt und entdecke die Welt auf deine eigene Art und
+          Weise.
+        </p>
+
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <button className={styles.stickyButton}>Jetzt registrieren</button>
       </div>
     </div>
   );
