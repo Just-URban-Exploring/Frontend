91b906de2b56df82b2f64db99aebddd9dc8806f0
Jackeine-Matos <jackeline.matos@gmail.com>
2023-02-28 11:20:59 +0100

    Styling AGB-Page

--- src/pages/Datenschutz.jsx
+++ src/pages/Datenschutz.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "../css/Datenschutz.module.css";
+import styles from "../css/Datenschutz-AGB.module.css";
 const Datenschutz = () => {
   const navigate = useNavigate();
 
