90b5c0372a7588de96e8e4dd5253124f15772b89
X-Ella-X <e.felsinger@web.de>
2023-03-01 22:09:04 +0100

    finished agb for medium devices

--- src/components/Audioguide.jsx
+++ src/components/Audioguide.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import { MdHeadset, MdHeadsetOff } from "react-icons/md";
-import '../css/audioguide.css';
 
 
 
--- src/pages/AGB.jsx
+++ src/pages/AGB.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/Global.css";
-import styles from "../css/Datenschutz-AGB.module.css";
+import styles from "../css/DatenschutzTemplate.module.css";
 
 const AGB = () => {
   const navigate = useNavigate();
@@ -15,29 +15,33 @@ const AGB = () => {
         Home
       </button>
       <div className={styles.AGBContainer}>
-        <h2>Allgemeine Geschäftsbedingungen Stand: </h2>
-        <h3>Februar 2023 Rechtliche Hinweise zur Benutzung: </h3>
-        <p>
-          {" "}
+        <h2 className={styles.h2Big}>Allgemeine Geschäftsbedingungen</h2>
+        <h4 className={styles.h4Bold}>Stand: Februar 2023</h4>
+        <p className={styles.pBold}>Rechtliche Hinweise zur Benutzung:</p>
+        <p className={styles.pText}>
           Der Unternehmer schließt im Laufe seiner Geschäftstätigkeit eine
           Vielzahl von Verträgen ab. Um eine Orientierungshilfe zu bieten,
-          stellt die IHK München und Oberbayern Musterverträge zur Verfügung.
-          Dieses Vertragsformular wurde mit größter Sorgfalt erstellt, erhebt
-          aber keinen Anspruch auf Vollständigkeit und Richtigkeit. Es ist als
-          Checkliste mit Formulierungshilfen zu verstehen und soll nur eine
-          Anregung bieten, wie die typische Interessenlage zwischen den Parteien
-          sachgerecht ausgeglichen werden kann. Dies entbindet den Verwender
-          jedoch nicht von der sorgfältigen eigenverantwortlichen Prüfung. Aus
-          Gründen der sprachlichen Vereinfachung wird auf die Nennung der
-          Geschlechter verzichtet, wo eine geschlechtsneutrale Formulierung
-          nicht möglich war. In diesen Fällen beziehen die verwendeten
-          männlichen Begriffe die weiblichen Formen ebenso mit ein. Der
-          Mustervertrag ist nur ein Vorschlag für eine mögliche Regelung. Viele
-          Festlegungen sind frei vereinbar. Der Verwender kann auch andere
-          Formulierungen wählen. Vor einer Übernahme des unveränderten Inhaltes
-          muss daher im eigenen Interesse genau überlegt werden, ob und in
-          welchen Teilen gegebenenfalls eine Anpassung an die konkret zu
-          regelnde Situation und die Rechtsentwicklung erforderlich ist. Auf
+          stellt die IHK München und Oberbayern Musterverträge zur Verfügung.</p>
+          <p className={styles.pText}>
+            Dieses Vertragsformular wurde mit größter Sorgfalt erstellt, erhebt
+            aber keinen Anspruch auf Vollständigkeit und Richtigkeit. Es ist als
+            Checkliste mit Formulierungshilfen zu verstehen und soll nur eine
+            Anregung bieten, wie die typische Interessenlage zwischen den Parteien
+            sachgerecht ausgeglichen werden kann. Dies entbindet den Verwender
+            jedoch nicht von der sorgfältigen eigenverantwortlichen Prüfung.
+          </p>
+          <p className={styles.pText}>Aus Gründen der sprachlichen Vereinfachung wird auf die Nennung der
+            Geschlechter verzichtet, wo eine geschlechtsneutrale Formulierung
+            nicht möglich war. In diesen Fällen beziehen die verwendeten
+            männlichen Begriffe die weiblichen Formen ebenso mit ein.
+          </p><p className={styles.pText}>
+            Der Mustervertrag ist nur ein Vorschlag für eine mögliche Regelung. Viele
+            Festlegungen sind frei vereinbar. Der Verwender kann auch andere
+            Formulierungen wählen. Vor einer Übernahme des unveränderten Inhaltes
+            muss daher im eigenen Interesse genau überlegt werden, ob und in
+            welchen Teilen gegebenenfalls eine Anpassung an die konkret zu
+            regelnde Situation und die Rechtsentwicklung erforderlich ist.
+          </p><p className={styles.pText}>Auf
           diesen Vorgang hat die Industrie- und Handelskammer natürlich keinen
           Einfluss und kann daher naturgemäß für die Auswirkungen auf die
           Rechtsposition der Parteien keine Haftung übernehmen. Auch die Haftung
@@ -45,37 +49,48 @@ const AGB = () => {
           einen maßgeschneiderten Vertrag benötigen, sollten Sie sich durch
           einen Rechtsanwalt Ihres Vertrauens beraten lassen.
         </p>
-        <h2>Allgemeine Geschäftsbedingungen für Tour Guide </h2>
-        <li> 1. Geltungsbereich </li>
-        <li> 2.Vertragspartner </li>
-        <li> 3. Vertragsschluss</li>
-        <li>4. Widerrufsrecht </li>
-        <li> 5. Preise und Versandkosten </li>
-        <li> 6.Lieferung </li>
-        <li>7. Zahlung </li>
-        <li> 8. Eigentumsvorbehalt </li>
-        <li> 9. Streitbeilegung Allgemeine Geschäftsbedingungen </li>
-        {/*         1. Geltungsbereich Für alle Lieferungen von DemoShop e.K. (nachfolgend
-        DemoShop) an Verbraucher gelten diese Allgemeinen Geschäftsbedingungen
-        (AGB). Verbraucher ist jede natürliche Person, die ein Rechtsgeschäft zu
-        einem Zwecke abschließt, der überwiegend weder ihrer gewerblichen noch
-        ihrer selbstständigen beruflichen Tätigkeit zugerechnet werden kann. 2.
-        Vertragspartner Der Kaufvertrag kommt zustande mit DemoShop e.K.,
-        Inhaber: Max Muster, Demostr. 1, 12345 Demostadt, Handelsregister:
-        Amtsgericht Demostadt, HRA 12345. 3. Vertragsschluss 3.1. Die
-        Darstellung der Produkte im Online-Shop stellt kein rechtlich bindendes
-        Angebot, sondern nur eine Aufforderung zur Bestellung dar. 3.2. Durch
+        <h2 className={styles.h2Big2}>Allgemeine Geschäftsbedingungen für Tour Guide App</h2>
+        <li className={styles.liStyle}> 1. Geltungsbereich </li>
+        <li className={styles.liStyle}> 2. Vertragspartner </li>
+        <li className={styles.liStyle}> 3. Vertragsschluss </li>
+        <li className={styles.liStyle}> 4. Widerrufsrecht </li>
+        <li className={styles.liStyle}> 5. Preise und Versandkosten </li>
+        <li className={styles.liStyle}> 6. Lieferung </li>
+        <li className={styles.liStyle}> 7. Zahlung </li>
+        <li className={styles.liStyle}> 8. Eigentumsvorbehalt </li>
+        <li className={styles.liStyle}> 9. Streitbeilegung Allgemeine Geschäftsbedingungen </li>
+        <p className={styles.pTitle}>Allgemeine Geschäftsbedingungen</p>
+          <li className={styles.liStyle}>1. Geltungsbereich</li> <p className={styles.pText}>
+            Für alle Lieferungen von Tour Guide App (nachfolgend
+                    JURE) an Verbraucher gelten diese Allgemeinen Geschäftsbedingungen
+                    (AGB). Verbraucher ist jede natürliche Person, die ein Rechtsgeschäft zu
+                    einem Zwecke abschließt, der überwiegend weder ihrer gewerblichen noch
+                    ihrer selbstständigen beruflichen Tätigkeit zugerechnet werden kann.
+          </p> 
+        <li className={styles.liStyle}>2. Vertragspartner
+        </li> <p className={styles.pText}>
+          Der Kaufvertrag kommt zustande mit Tour Guide App,
+          Inhaber: Max Muster, Demostr. 1, 12345 Demostadt, Handelsregister:
+          Amtsgericht Demostadt, HRA 12345.
+        </p>
+        <li className={styles.liStyle}>3. Vertragsschluss</li>
+        <li className={styles.liStyle2}>3.1. Die Darstellung
+        der Produkte in der App stellt kein rechtlich bindendes Angebot, sondern nur eine Aufforderung zur Bestellung dar.</li>
+        <li className={styles.liStyle2}>3.2. Durch
         Anklicken des Buttons [Kaufen/kostenpflichtig bestellen] geben Sie eine
         verbindliche Bestellung der auf der Bestellseite aufgelisteten Waren ab.
         Ihr Kaufvertrag kommt zustande, wenn wir Ihre Bestellung durch eine
         Auftragsbestätigung per E-Mail unmittelbar nach dem Erhalt Ihrer
-        Bestellung annehmen. 4. Widerrufsrecht 4.1. Wenn Sie Verbraucher sind
+        Bestellung annehmen.</li>
+        <li className={styles.liStyle}>4. Widerrufsrecht</li>
+        <li className={styles.liStyle2}>4.1. Wenn Sie Verbraucher sind
         (also eine natürliche Person, die die Bestellung zu einem Zweck abgibt,
         der weder Ihrer gewerblichen oder selbständigen beruflichen Tätigkeit
         zugerechnet werden kann), steht Ihnen nach Maßgabe der gesetzlichen
-        Bestimmungen ein Widerrufsrecht zu. 4.2. Machen Sie als Verbraucher von
+        Bestimmungen ein Widerrufsrecht zu.</li>
+        <li className={styles.liStyle2}>4.2. Machen Sie als Verbraucher von
         Ihrem Widerrufsrecht nach Ziffer 4.1 Gebrauch, so haben Sie die
-        regelmäßigen Kosten der Rücksendung zu tragen. 4.3. Im Übrigen gelten
+        regelmäßigen Kosten der Rücksendung zu tragen.</li> <li className={styles.liStyle2}>4.3. Im Übrigen gelten
         für das Widerrufsrecht die Regelungen, die im Einzelnen wiedergegeben
         sind in der folgenden Widerrufsbelehrung Widerrufsrecht Sie haben das
         Recht, binnen vierzehn Tagen ohne Angabe von Gründen diesen Vertrag zu
@@ -83,10 +98,8 @@ const AGB = () => {
         Sie oder ein von Ihnen benannter Dritter, der nicht der Beförderer ist,
         die Waren in Besitz genommen haben bzw. hat. Um Ihr Widerrufsrecht
         auszuüben, müssen Sie uns [Namen des Unternehmers, Anschrift,
-        Telefonnummer und E-Maimport styles from */}
-        <h2>Allgemeine Geschäftsbedingungen für Tour Guide II </h2>
-        <p>
-          "../css/Datenschutz-AGB.module.css";il Adresse eintragen] mittels
+        Telefonnummer und E-Mail Adresse eintragen]
+        mittels
           einer eindeutigen Erklärung (zB ein mit der Post versandter Brief oder
           eine E-Mail) über Ihren Entschluss, diesen Vertrag zu widerrufen,
           informieren. Sie können dafür das beigefügte Muster-Widerrufsformular
@@ -122,69 +135,47 @@ const AGB = () => {
           Rücksendung der Waren. Sie müssen für einen etwaigen Wertverlust der
           Waren nur aufkommen, wenn dieser Wertverlust auf einen zur Prüfung der
           Beschaffenheit, Eigenschaften und Funktionsweise der Waren nicht
-          notwendigen Umgang mit ihnen zurückzuführen ist. - Ende der
-          Widerrufsbelehrung - 5. Preise und Versandkosten 5.1. Die auf den
+          notwendigen Umgang mit ihnen zurückzuführen ist.</li> <p className={styles.pBold}>
+            - Ende der
+            Widerrufsbelehrung -
+          </p> 
+          <li className={styles.liStyle}>5. Preise und Versandkosten</li>
+          <li className={styles.liStyle2}>5.1.Die auf den
           Produktseiten genannten Preise enthalten die gesetzliche
-          Mehrwertsteuer und sonstige Preisbestandteile. 5.2. Zusätzlich zu den
+          Mehrwertsteuer und sonstige Preisbestandteile.</li>
+          <li className={styles.liStyle2}>5.2. Zusätzlich zu den
           angegebenen Preisen berechnen wir für die Lieferung innerhalb
           Deutschlands pauschal ____ Euro pro Bestellung. Die Versandkosten
           werden Ihnen auf den Produktseiten, im Warenkorbsystem und auf der
-          Bestellseite nochmals deutlich mitgeteilt. 5.3. Bei Zahlung per
+          Bestellseite nochmals deutlich mitgeteilt.</li>
+          <li className={styles.liStyle2}>5.3. Bei Zahlung per
           Nachnahme wird zusätzlich eine Gebühr in Höhe von ____ Euro fällig,
           die der Zusteller vor Ort erhebt. Weitere Steuern oder Kosten fallen
-          nicht an. 6. Lieferung 6.1. Die Lieferung erfolgt nur innerhalb
-          Deutschlands mit ____. 6.2. Die Lieferzeit beträgt bis zu 3 Tage. Auf
+          nicht an.</li>
+          <li className={styles.liStyle}>6. Lieferung</li>
+          <li className={styles.liStyle2}>6.1. Die Lieferung erfolgt nur innerhalb
+          Deutschlands mit ____.</li>
+          <li className={styles.liStyle2}>6.2. Die Lieferzeit beträgt bis zu 3 Tage. Auf
           eventuell abweichende Lieferzeiten weisen wir auf der jeweiligen
-          Produktseite hin. 7. Zahlung 7.1. Die Zahlung erfolgt wahlweise per
-          Vorkasse oder Nachnahme. 7.2. Bei Auswahl der Zahlungsart Vorkasse
-          nennen.
-        </p>
-        <span>
-          Quelle: Online-Handel, Wegweiser durch die rechtlichen
-          Rahmenbedingungen des E-Commerce unter Berücksichtigung des neuen
-          Verbraucherrechts, von Rechtsanwalt Dr. Carsten Föhlisch/Trusted Shops
-          GmbH und Rechtsanwalt Dr. Christian Groß/DIHK Deutscher Industrie-und
-          Handelskammertag, DIHK Verlag, 2.Auflage 2018 (www.dihk-verlag.de )
-          Hinweis: Die Veröffentlichung des Musters erfolgt mit freundlicher
-          Genehmigung der Autoren. Die Widerrufsbelehrung ist aktualisiert und
-          entspricht dem aktuellen Stand ab 28.05.2022 gemäß Anlage 1 zu Art.
-          246 a § 1 Absatz 2 Satz 2 EGBGB. (Anl. 1 neu gef. mWv 13.6.2014 durch
-          G v. 20.9.2013 (BGBl. I S. 3642); geänd. mWv 21.3.2016 durch G v.
-          11.3.2016 (BGBl. I S 396); geänd. mWv 28.5.2022 durch G v. 10.8.2021
-          (BGBl. I S. 3483)).
-        </span>
-        Streitigkeiten geschaffen. Die Plattform dient als Anlaufstelle zur
-        außergerichtlichen Beilegung von Streitigkeiten betreffend vertragliche
-        Verpflichtungen, die aus Online-Kaufverträgen erwachsen. Nähere
-        Informationen sind unter dem folgenden Link verfügbar:
-        http://ec.europa.eu/consumers/odr. Wir sind zur Beilegung von
-        Streitigkeiten mit Verbrauchern zur Teilnahme an einem
-        Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle bereit
-        oder gemäß ____ (Angabe der Rechtsnorm oder der vertraglichen
-        Vereinbarung) verpflichtet. Die zuständige Verbraucherschlichtungsstelle
-        ist: Universalschlichtungsstelle des Bundes Zentrum für Schlichtung
-        e.V., Straßburger Straße 8, 77694 Kehl am Rhein,
-        www.verbraucher-schlichter.de. Zur Beilegung der genannten
-        Streitigkeiten werden wir in einem Streitbeilegungsverfahren vor dieser
-        Stelle teilnehmen. Alternativ: Die EU-Kommission hat eine
-        Internetplattform zur Online-Beilegung von Streitigkeiten geschaffen.
-        Die Plattform dient als Anlaufstelle zur außergerichtlichen Beilegung
-        von Streitigkeiten betreffend vertragliche Verpflichtungen, die aus
-        Online-Kaufverträgen erwachsen. Nähere Informationen sind unter dem
-        folgenden Link verfügbar: http://ec.europa.eu/consumers/odr. Zur
-        Teilnahme an einem Streitbeilegungsverfahren vor einer
-        Verbraucherschlichtungsstelle sind wir weder bereit noch verpflichtet.
-        Quelle: Online-Handel, Wegweiser durch die rechtlichen Rahmenbedingungen
-        des E-Commerce unter Berücksichtigung des neuen Verbraucherrechts, von
-        Rechtsanwalt Dr. Carsten Föhlisch/Trusted Shops GmbH und Rechtsanwalt
-        Dr. Christian Groß/DIHK Deutscher Industrie-und Handelskammertag, DIHK
-        Verlag, 2.Auflage 2018 (www.dihk-verlag.de ) Hinweis: Die
-        Veröffentlichung des Musters erfolgt mit freundlicher Genehmigung der
-        Autoren. Die Widerrufsbelehrung ist aktualisiert und entspricht dem
-        aktuellen Stand ab 28.05.2022 gemäß Anlage 1 zu Art. 246 a § 1 Absatz 2
-        Satz 2 EGBGB. (Anl. 1 neu gef. mWv 13.6.2014 durch G v. 20.9.2013 (BGBl.
-        I S. 3642); geänd. mWv 21.3.2016 durch G v. 11.3.2016 (BGBl. I S 396);
-        geänd. mWv 28.5.2022 durch G v. 10.8.2021 (BGBl. I S. 3483)).
+          Produktseite hin.</li>
+          <li className={styles.liStyle}>7. Zahlung</li>
+          <li className={styles.liStyle2}>7.1. Die Zahlung erfolgt wahlweise per
+          Vorkasse oder Nachnahme.</li>
+          <li className={styles.liStyle2}>7.2. Bei Auswahl der Zahlungsart Vorkasse nennen wir Ihnen unsere Bankverbindung in der Auftragsbestätigung und liefern die Ware nach Zahlungseingang.</li>
+        <li className={styles.liStyle}>8. Eigentumvorbehalt</li>
+        <p className={styles.pText}>Bis zur vollständigen Zahlung bleibt die Ware unser Eigentum.</p>
+        <li className={styles.liStyle}>9. Streitbeilegung</li>
+        <p className={styles.pText}>Die EU-Kommission hat eine Internetplattform zur Online-Beilegung von Streitigkeiten geschaffen. Die Plattform dient als Anlaufstelle zur außergerichtlichen Beilegung von Streitigkeiten betreffend vertragliche Verpflichtungen, die aus Online-Kaufverträgen erwachsen. Nähere Informationen sind unter dem folgenden Link verfügbar: http://ec.europa.eu/consumers/odr. Wir sind zur Beilegung von Streitigkeiten mit Verbrauchern zur Teilnahme an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle bereit oder gemäß ____ (Angabe der Rechtsnorm oder der vertraglichen Vereinbarung) verpflichtet. Die zuständige Verbraucherschlichtungsstelle ist: Universalschlichtungsstelle des Bundes Zentrum für Schlichtung e.V., Straßburger Straße 8, 77694 Kehl am Rhein, www.verbraucher-schlichter.de. Zur Beilegung der genannten Streitigkeiten werden wir in einem Streitbeilegungsverfahren vor dieser Stelle teilnehmen.</p>
+        <span className={styles.spanKursiv}>Alternativ:</span>
+        <p className={styles.pText}>Die EU-Kommission hat eine Internetplattform zur Online-Beilegung von Streitigkeiten geschaffen. Die Plattform dient als Anlaufstelle zur außergerichtlichen Beilegung von Streitigkeiten betreffend vertragliche Verpflichtungen, die aus Online-Kaufverträgen erwachsen. Nähere Informationen sind unter dem folgenden Link verfügbar:
+http://ec.europa.eu/consumers/odr.  Zur Teilnahme an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle sind wir weder bereit noch verpflichtet.</p>
+        <div className={styles.lastDiv}>
+          <p className={styles.pTextDiv1}>
+          Quelle: Online-Handel, Wegweiser durch die rechtlichen Rahmenbedingungen des E-Commerce unter Berücksichtigung des neuen Verbraucherrechts, von Rechtsanwalt Dr. Carsten Föhlisch/Trusted Apps GmbH und Rechtsanwalt Dr. Christian Groß/DIHK Deutscher Industrie-und Handelskammertag, DIHK Verlag, 2.Auflage 2018 (www.dihk-verlag.de)
+          </p>
+          <p className={styles.pTextDiv2}>Hinweis: Die Veröffentlichung des Musters erfolgt mit freundlicher Genehmigung der Autoren. 
+          Die Widerrufsbelehrung ist aktualisiert und entspricht dem aktuellen Stand ab 28.05.2022 gemäß Anlage 1 zu Art. 246 a § 1 Absatz 2 Satz 2 EGBGB. (Anl. 1 neu gef. mWv 13.6.2014 durch G v. 20.9.2013 (BGBl. I S. 3642); geänd. mWv 21.3.2016 durch G v. 11.3.2016 (BGBl. I S 396); geänd. mWv 28.5.2022 durch G v. 10.8.2021 (BGBl. I S. 3483)).</p>
+        </div>
       </div>
     </div>
   );
